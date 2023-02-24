aa97b93ef4a9e94c112ecd137984dfbd6ea56b50
Zander <114107976+Femnyx@users.noreply.github.com>
2023-02-24 12:56:32 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Variables_Mcvey_Zander
+ 
---
+++ Variables_Mcvey_Zander/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Variables_Mcvey_Zander
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
