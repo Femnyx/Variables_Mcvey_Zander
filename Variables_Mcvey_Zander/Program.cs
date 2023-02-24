ebf49a8c1e3087b6df3ee3aff4c7d1d88428376a
Zander <114107976+Femnyx@users.noreply.github.com>
2023-02-24 14:37:29 -0600

    Update 1.2 same day same thing

--- Variables_Mcvey_Zander/Program.cs
+++ Variables_Mcvey_Zander/Program.cs
@@ -5,19 +5,27 @@ namespace Variables_Mcvey_Zander
     class Program
     {
         static void Main(string[] args)
-        {
+        {  /*Integer and is labeled "favoriteNumber"*/
             int favoriteNumber;
+            /*They will not be jumping or running through the text */
             bool isJumping = false, isRunning = false;
+            /*myFloat is the floating point which is stated to be 89.12*/
             float myFloat;
+            /*This is my favorite number, 7*/
             favoriteNumber = 7;
+            /*Shows myFloat is the number 89.12 for this program */
             myFloat = 89.12f;
+            /*The constant is finalGrade, which is a double for this program */
             const double finalGrade = 87.12;
 
-            Console.WriteLine($"The lucky number is {favoriteNumber}");
-            Console.WriteLine($"it's {isJumping}");
-            Console.WriteLine($"it is not {isRunning}!");
-            Console.WriteLine($"i'm not! 2 years ago it was {myFloat}!");
-            Console.WriteLine($"first off that is the wrong number and second my favorite number back then was {finalGrade}!");
+            /*STOP READING ME MR.HOUSTON!*/
+
+            /*Prints all the variables to console */
+            Console.WriteLine("Favorite #: " + favoriteNumber);
+            Console.WriteLine("Jumping: " + isJumping);
+            Console.WriteLine("Running: " + isRunning);
+            Console.WriteLine("Random Floating #: " + myFloat);
+            Console.WriteLine("Final Grade " + finalGrade);
 
         }
     }
