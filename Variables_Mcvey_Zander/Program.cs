e58de047340353f8f0750ad34b34471b33b85505
Zander <114107976+Femnyx@users.noreply.github.com>
2023-02-24 13:13:35 -0600

    Variable update v1.1, The Big numbers!

--- Variables_Mcvey_Zander/Program.cs
+++ Variables_Mcvey_Zander/Program.cs
@@ -6,7 +6,19 @@ namespace Variables_Mcvey_Zander
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int favoriteNumber;
+            bool isJumping = false, isRunning = false;
+            float myFloat;
+            favoriteNumber = 7;
+            myFloat = 89.12f;
+            const double finalGrade = 87.12;
+
+            Console.WriteLine($"The lucky number is {favoriteNumber}");
+            Console.WriteLine($"it's {isJumping}");
+            Console.WriteLine($"it is not {isRunning}!");
+            Console.WriteLine($"i'm not! 2 years ago it was {myFloat}!");
+            Console.WriteLine($"first off that is the wrong number and second my favorite number back then was {finalGrade}!");
+
         }
     }
 }
